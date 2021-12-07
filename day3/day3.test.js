f1ce794ba32d1250db81ba88b21fe7d1de458dd7
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2021-12-08 09:55:24 +1100

    refactor tests to table driven tests

--- day3/day3.test.js
+++ day3/day3.test.js
@@ -1,56 +1,62 @@
 const { transpose, powerConsumption } = require("./utils");
 
 describe("transpose", () => {
-  it("test1", () => {
-    const input = [
-      [1, 2, 3],
-      [1, 2, 3],
-      [1, 2, 3],
-    ];
-    const expected = [
-      [1, 1, 1],
-      [2, 2, 2],
-      [3, 3, 3],
-    ];
-    const actual = transpose(input);
-    expect(actual).toEqual(expected);
-  });
-  it("test1", () => {
-    const input = [
-      [1, 2, 3, 4],
-      [1, 2, 3, 4],
-      [1, 2, 3, 4],
-      [1, 2, 3, 4],
-    ];
-    const expected = [
-      [1, 1, 1, 1],
-      [2, 2, 2, 2],
-      [3, 3, 3, 3],
-      [4, 4, 4, 4],
-    ];
-    const actual = transpose(input);
-    expect(actual).toEqual(expected);
+  const tests = [
+    {
+      name: "test1",
+      input: [
+        [1, 2, 3],
+        [1, 2, 3],
+        [1, 2, 3],
+      ],
+      expected: [
+        [1, 1, 1],
+        [2, 2, 2],
+        [3, 3, 3],
+      ],
+    },
+    {
+      name: "test2",
+      input: [
+        [1, 2, 3, 4],
+        [1, 2, 3, 4],
+        [1, 2, 3, 4],
+        [1, 2, 3, 4],
+      ],
+      expected: [
+        [1, 1, 1, 1],
+        [2, 2, 2, 2],
+        [3, 3, 3, 3],
+        [4, 4, 4, 4],
+      ],
+    },
+  ];
+
+  tests.forEach((test) => {
+    it(test.name, () => {
+      expect(transpose(test.input)).toEqual(test.expected);
+    });
   });
 });
 
 describe("power consumption", () => {
-  it("returns for 1 line", () => {
-    var input = `00100`;
-    var expected = 4 * 27;
-    var actual = powerConsumption(input);
-    expect(actual).toEqual(expected);
-  });
-  it("returns for 3 lines", () => {
-    var input = `00100
+  const tests = [
+    {
+      name: "returns for 1 line",
+      input: `00100`,
+      expected: 4 * 27,
+    },
+    {
+      name: "returns for 3 lines",
+      input: `00100
 11110
 10110
-`;
-    var expected = 22 * 9; // 10110
-    var actual = powerConsumption(input);
-    expect(actual).toEqual(expected);
-  });
-  it("returns for example lines", () => {
-    var input = `00100
+`,
+      expected: 22 * 9,
+    },
+    {
+      name: "returns for example lines",
+      input: `00100
 11110
 10110
 10111
@@ -62,9 +68,14 @@ describe("power consumption", () => {
 11001
 00010
 01010
-`;
-    var expected = 22 * 9; // 10110
-    var actual = powerConsumption(input);
-    expect(actual).toEqual(expected);
+`,
+      expected: 22 * 9,
+    },
+  ];
+
+  tests.forEach((test) => {
+    it(test.name, () => {
+      expect(powerConsumption(test.input)).toEqual(test.expected);
+    });
   });
 });
